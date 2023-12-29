9fa04adc2c408e12b8ffebbabf1ac0963f79d865
MdSagorMunshi <mi.sagor56@gmail.com>
2023-12-29 08:35:26 -0600

    fix error (:

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -17,7 +17,11 @@ export const getTotalRevenue = async (storeId: string) => {
 
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber();
+      // Use Number() or parseFloat() to convert the price to a number
+      const itemPrice = Number(item.product.price);
+      // Or: const itemPrice = parseFloat(item.product.price);
+
+      return orderSum + itemPrice;
     }, 0);
     return total + orderTotal;
   }, 0);
